5ee1ec816aaaf76f8c04f40a5092231fe407790a
Will Smith <william.smith@service.nsw.gov.au>
2021-03-19 11:38:17 +1100

    add endpoint

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString(); // Hypothetical synchronous operation
+    next();
+    }, function(req, res) {
+    res.send({time: req.time});
+    });
 console.log('Hello World')
 
 
